d0f53a7f3f48091367153687cf258ba65c20e3ef
Abe Massry <a@abemassry.com>
2014-01-28 16:49:39 -0500

    working on hashmaps implementation

--- ex37/liblcthw/src/hashmap.c
+++ ex37/liblcthw/src/hashmap.c
@@ -10,4 +10,28 @@ static int default_compare(void *a, void *b)
 }
 
 /**
- * Simple
+ * Simple Bob Jenkins's hash algorithm taken from the
+ * wikipedia description
+ */
+static uint32_t default_hash(void *a)
+{
+  size_t len = blength((bstring)a);
+  char *key = bdaya((bstring)a);
+  uint32_t hash = 0;
+  uint32_t i = 0;
+
+  for(hash = i = 0; i < len; ++i)
+  {
+    hash += key[i];
+    hash += (hash << 10);
+    hash ^= (hash >> 6);
+  }
+
+  hash += (hash << 3);
+  hash ^= (hash >> 11);
+  hash += (hash << 15);
+
+  return hash;
+}
+
+Hashmap *Hashmap_create(
