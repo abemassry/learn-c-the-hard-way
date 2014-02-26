39df1bb2570b56d78b93ad8c8f640c2fda9ec04c
Abe Massry <a@abemassry.com>
2014-02-26 13:33:32 -0500

    asm in c fixes

--- asm_in_c.c
+++ asm_in_c.c
@@ -2,8 +2,8 @@
 
 int main() {
   // Add 10 and 20 and store result into register %eax
-  __asm__ ( "movl, $10, %eax;"
-            "movl, $20, %ebx;"
+  __asm__ ( "movl $10, %eax;"
+            "movl $20, %ebx;"
             "addl %ebx, %eax;"
   );
 
